34f37fe525f8a28b97df90ec77d4cd77ac7b326c
camilito2005 <134243118+camilito2005@users.noreply.github.com>
2023-10-05 18:36:20 -0500

    Add files via upload

---
+++ inicio/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEO INVENTORY</title>
+</head>
+<body>
+    <div class="container">
+        <div class="links">
+            <a href="../ferreteria_neo/public/">Inventario</a>
+            <a href="../ferrinventadso/public/">Perfil</a>
+        </div>
+    </div>
+</body>
+<style>
+body{
+    background: black;
+    margin: 0;
+
+}
+.container{
+    width: 100%;
+
+}
+a{
+    color: white;
+    text-decoration: none;
+    font-size: 20px;
+    padding: 10px;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+}
+a::before{
+    color: red;
+}
+a:hover{
+    color: red;
+}
+.links{
+    float: right;
+    margin-top: 40px;
+}
+</style>
+</html>
